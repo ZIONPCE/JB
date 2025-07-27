2e3a45697356301fe13dfed43d15f10a816df716
ZIONPCE <3891722440@qq.com>
2025-07-27 16:03:47 +0800

    Add files via upload

---
+++ ght.lua
@@ -0,0 +1,35 @@
+loadstring(game:HttpGet("https://rifton.top/loader.lua"))()
+
+local function main(v)
+    if floor.Value == "Fools" then
+        if v:IsA("TextLabel") or v:IsA("TextButton") then
+            repeat task.wait() until v.Text ~= "Label"
+        end
+    end
+    if v:IsA("TextLabel") then
+        if name[v.Text] then
+            v.Text = name[v.Text]
+        elseif v.Text == "Speed Boost : 6" then
+            v.Text = "速度提升 : 6"
+            v:GetPropertyChangedSignal("Text"):Connect(function()
+                textnumber = string.match(v.Text, "Speed Boost : (%w+)")
+                if textnumber then
+                    v.Text = "速度提升 : "..textnumber
+                end
+            end)
+        elseif v.Text == "Max Slope Angle : 49" then
+            v.Text = "最大倾斜角 : 49"
+            v:GetPropertyChangedSignal("Text"):Connect(function()
+                textnumber = string.match(v.Text, "Max Slope Angle : (%w+)")
+                if textnumber then
+                    v.Text = "最大倾斜角 : "..textnumber
+                end
+            end)
+        elseif v.Text == "Field of View : 70" then
+            v.Text = "视野 : 70"
+            v:GetPropertyChangedSignal("Text"):Connect(function()
+                textnumber = string.match(v.Text, "Field of View : (%w+)")
+                if textnumber then
+                    v.Text = "视野 : "..textnumber
+                end
+            end)
